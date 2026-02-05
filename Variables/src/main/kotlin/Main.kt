77e6dda8159ade4222e34dfd3b450234c2ac22b2
Abdimajid Dakane <muktaralu114@gmail.com>
2026-02-05 19:47:48 +0300

    Variables in kotlin

---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+   var userName = "Abdi"
+   userName = "Abdimajid"
+   userName = "John"
+   var age = 22
+   age = 25
+   age = 1
+   println("Hello $userName you are $age years old")
+}
